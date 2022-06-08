08b5e99226d59249221577381b68ee8fa20eb254
Shawn Lee <Shawnl93@hotmail.com>
2022-06-08 14:11:55 -0600

    Add more complicated friend detector

--- friends.rb
+++ friends.rb
@@ -1,7 +1,12 @@
-bestie = "Shawn"
+bestie = "Eric"
+name = "Roy"
 
-if bestie == "Eric"
+if bestie == "Eric" && name != "Roy"
   puts "yay bestie"
+elsif bestie == "Eric" && name == "Roy"
+  puts "They are both here yay"
+elsif name == "Roy" && bestie != "Eric"
+  puts "Still good though"
 else
   puts "not bestie"
 end
